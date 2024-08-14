a45f96cdba13e0498d5ed21b17cb664e9f356e90
bogdan <bogdanbilik2000@gmail.com>
2024-08-14 17:13:17 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from PyQt5.QtWidgets import QApplication
 from time import sleep
 
+
 app = QApplication([])
 
-
-from main_window import*
-from menu_window import*
-
+from main_window import *
+from menu_window import *
 class Question():
     def __init__(self, question, answer, wrong_ans1, wrong_ans2, wrong_ans3):
         self.question = question 
@@ -21,21 +20,18 @@ class Question():
         self.count_asked += 1
         self.count_right += 1
     def got_wrong(self):
-        self.count_asked += 1
-
-q1 = Question("Яблуко", "apple", "sssss", "dddd", "aaaaa")
-q2 =
-q3 
-
+            self.count_asked += 1
+q1 = Question("Яблуко", "apple", "pinapple", "apply", "application")
+q2 = Question("Дім", "house", "horse", "hobbi", "hang")
+q3 = Question("Новий", "new", "know", "never", "need")
+q4 = Question("Школа", "school", "sky", "sun", "snow")
 
 
 def back_menu():
     menu_w.hide()
     window.show()
 
-
 btn_back.clicked.connect(back_menu)
-
 def rest():
     window.hide()
     n = sp_rest.value() * 60
@@ -43,18 +39,6 @@ def rest():
     window.show()
 
 btn_rest.clicked.connect(rest)
-
-
-v1_lineEdits = QVBoxLayout()
-v1_lineEdits.addWidget(le_question)
-v1_lineEdits.addWidget(le_right_ans)
-v1_lineEdits.addWidget(le_wrong_ans1)
-v1_lineEdits.addWidget(le_wrong_ans2)
-v1_lineEdits.addWidget(le_wrong_ans3)
-
-
-
-
 
 menu_w.show()
 app.exec_()
